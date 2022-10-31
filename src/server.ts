6184abcffe6d80364e1118ab4e7cf14712894ed8
Felipe Lima <felipelima@ioasys.com.br>
2022-10-31 15:25:29 -0300

    init

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: `server is running on port ${PORT} aaaaaaaaa` })
+});
+
+app.use(route);
+
+app.listen(PORT)
