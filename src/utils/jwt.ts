6ce2884b5a293062116234e0bddf3d6213a5148d
Felipe Lima <felipelima@ioasys.com.br>
2022-11-02 23:14:37 -0300

    fix jwt password env

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,23 +1,16 @@
 import * as jwt from 'jsonwebtoken';
-import { readFileSync } from 'fs';
 import { TokenData } from '../interfaces/Token';
 
 export default class JWT {
-  private _password: string;
-
-  constructor() {
-    this._password = readFileSync('jwt.evaluation.key', { encoding: 'utf8' });
-  }
-
   public sign(payload: TokenData) {
     return jwt.sign(
       payload,
-      this._password,
+      process.env.JWT_SECRET,
       { algorithm: 'HS256', expiresIn: '1h' }
     );
   }
 
   public verify(token: string) {
-    return jwt.verify(token, this._password, { algorithms: ['HS256'] });
+    return jwt.verify(token, process.env.JWT_SECRET, { algorithms: ['HS256'] });
   }
 }
