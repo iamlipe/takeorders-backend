d83a5a946c7581b1cbdf3334c9e125a482004c26
Felipe Lima <felipelima@ioasys.com.br>
2022-11-02 23:02:58 -0300

    fix connection

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -7,7 +7,7 @@ config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL
+      url: process.env.CONNECTION_STRING
     }
   }
 });
