4ce862f9db6a518f3ea9856bbb3d976b88dc7f99
Felipe Lima <felipelima@ioasys.com.br>
2022-11-04 16:58:36 -0300

    fix upload images remote

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ class App {
   private route(): void {
     this.app.use('/login', new LoginRouter().router);
     this.app.use('/register', new RegisterRouter().router);
-    this.app.use('/public/images', express.static(path.join(__dirname, "..", "public", "images")));
+    this.app.use(express.static('public'));
   
     this.app.use(Auth.headers());
     this.app.use(Auth.jwt());
